71bdfbe245b94aac89bade9a2f5e1dfc43319432
Dom Grad <Dominikgrad47@gmail.com>
2023-08-06 16:28:53 +0100

    adds astro build

--- src/components/FlashMessage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FlashMessage() {
-  return (
-    <div>FlashMessage</div>
-  )
-}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors:{
-        'grey': {
-          'dark': '#262626'
-        },
-        'red': {
-          'crimson': '#D7263D'
-        },
-        'primary': '#ec347a'
-      }
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
