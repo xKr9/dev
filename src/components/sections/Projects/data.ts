46fd4a343713da21fd6ab2d173e8d699ca4ae514
Dom Grad <Dominikgrad47@gmail.com>
2024-10-31 21:37:57 +0000

    adds more images

--- src/components/sections/Projects/data.ts
+++ src/components/sections/Projects/data.ts
@@ -8,8 +8,61 @@ import gotLearning from "../../../assets/imgs/GOT_IMG.png";
 import taxi from "../../../assets/imgs/TAXI_APP.png";
 import ultraleap from "../../../assets/imgs/ULTRALEAP_IMG.jpg";
 import beacon from "../../../assets/imgs/BEACON_IMG.jpg";
+import clearooms from "../../../assets/imgs/CLEAROOMS_IMG.jpg";
+import ama from "../../../assets/imgs/AMA_IMG.png";
+import pln from "../../../assets/imgs/PLN_IMG.png";
+import hat from "../../../assets/imgs/HATLESS_IMG.png";
+import pragma from "../../../assets/imgs/PRAGMA_IMG.jpg";
 
 const data = [
+  {
+    title: "Clearooms",
+    description: "A web application for booking desks and meeting rooms.",
+    pills: ["Vue", "Laravel", "Tailwind", "PHP"],
+    img: clearooms,
+    link: "/projects/clearooms",
+  },
+  {
+    title: "Pragma Holis",
+    description:
+      "A web application for businesses to generate an overview of their strengths and weaknesses.",
+    pills: ["React", "TypeScript", "Tailwind", "GraphQL"],
+    img: pragma,
+    link: "/projects/pragma-holis",
+  },
+  {
+    title: "Primary Languages Network",
+    description:
+      "A language learning application for children to practice their language listening, reading and writing skills via fun-to-play games, with a space theme.",
+    img: pln,
+    pills: ["Vue", "Django", "MySQL", "Tailwind", "JavaScript"],
+    link: "projects/primary-languages-network",
+  },
+  {
+    title: "A.M.A Selections",
+    description:
+      "Luxurious holiday booking application offering a wide range of privately owned luxuary villas in Europe.",
+    pills: [
+      "React",
+      "JavaScript",
+      "NextJS",
+      "Tailwind",
+      "PHP",
+      "Laravel",
+      "PostgreSQL",
+      "GraphQL",
+    ],
+    img: ama,
+    link: "projects/ama-selections",
+  },
+  {
+    title: "Hatless Website Redesign",
+    description:
+      "A collaborative project with the internal design team to redesign the Hatless website.",
+    pills: ["Vue", "Nuxt", "Tailwind", "Strapi", "JavaScript"],
+    img: hat,
+    link: "projects/hatless-website",
+  },
   {
     title: "Case Management Platform",
     description:
